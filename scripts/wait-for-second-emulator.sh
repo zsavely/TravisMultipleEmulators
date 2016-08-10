914106b06e8f0dfd1055afd1fd99b9413970f68b
Savelii Zagurskii <saveliyzagurskiy@gmail.com>
2016-08-10 12:49:13 +0300

    Changed 'android-wait-for-emulator' to custom script.

---
+++ scripts/wait-for-second-emulator.sh
@@ -0,0 +1,6 @@
+WAIT_CMD="$ANDROID_SDK/platform-tools/adb -s emulator-5556 wait-for-device shell getprop init.svc.bootanim"
+
+until $WAIT_CMD | grep -m 1 stopped; do
+  echo "Waiting for second emulator..."
+  sleep 1
+done
